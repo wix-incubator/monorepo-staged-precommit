d55e8448ec732b713a7151075937182d142d21b8
nachshons <nachshons@wix.com>
2018-07-04 00:34:57 +0300

    don't read precommit

--- index.js
+++ index.js
@@ -12,8 +12,6 @@ const gitDirectory = findUp.sync('.git');
 
 const preCommitFilePath = `${gitDirectory}/hooks/pre-commit`;
 
-const preCommitScript = fs.readFileSync(preCommitFilePath, 'utf-8');
-
 const {version} = require('./package.json');
 
 fs.writeFileSync(preCommitFilePath, getPrecommitScripts(version));
